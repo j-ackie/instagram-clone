ede3e78cb1a0f9b86f5385d9494b555d1d46b891
Jackie Lin <jackielin2025@u.northwestern.edu>
2022-08-06 16:30:57 -0400

    gitignore

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,14 +5,7 @@ import PostDataService from './services/post';
 function App() {
   return (
     <div className="App">
-      <button onClick={ () => {
-        PostDataService.createPos("hello")
-        .then(response => {
-          console.log(response.data);
-        });
-      } }>
-        hey
-      </button>
+    
     </div>
   );
 }
